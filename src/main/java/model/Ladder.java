965cb14b36d21357ba708674019d0def8bfa452b
asa9874 <pjkpjk1129@gmail.com>
2025-08-11 19:29:44 +0900

    feat : Ladder, Line 클래스 구현

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.List;
+
 public class Ladder {
-    
+    private final List<Line> lines;
+
+    public Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+
+    public boolean isPoint(int lineIndex, int pointIndex) {
+        if (lineIndex < 0 || lineIndex >= lines.size()) {
+            throw new IndexOutOfBoundsException("인덱스 범위 초과");
+        }
+        return lines.get(lineIndex).isPoint(pointIndex);
+    }
 }
--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.List;
+
 public class Line {
-    
+    private final List<Boolean> points;
+
+    public Line(List<Boolean> points) {
+        this.points = points;
+    }
+
+    public List<Boolean> getPoints() {
+        return points;
+    }
+
+    public boolean isPoint(int index) {
+        if (index < 0 || index >= points.size()) {
+            throw new IndexOutOfBoundsException("인덱스 범위 초과");
+        }
+        return points.get(index);
+    }
 }
